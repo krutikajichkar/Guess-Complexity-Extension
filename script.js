5b61f3d3bb2f66ae6c56f597c9a12d5883638ad6
Krutika Jichkar <krutikajichkar2@gmail.com>
2022-02-12 17:36:45 +0530

    Initil commit

---
+++ script.js
@@ -0,0 +1,439 @@
+const array = [
+    {
+        que:'What is the Average and Worst Case Time Complexity to access an element in Array?',
+        ans:'O(1)',
+       
+    },
+    {
+        que:'What is the Average and Worst Case Time Complexity to search an element in Array ?',
+        ans:'O(N)',
+       
+    },
+    {
+        que:'What is the Average and Worst Case Time Complexity to insert an element in Array?',
+        ans:'O(N)',
+       
+    },
+    {
+        que:'What is the Average and Worst Case Time Complexity to delete an element from Array?',
+        ans:'O(N)',
+       
+    },
+    {
+        que:'What is the Average and Worst Case Time Complexity to access an element in Stack?',
+        ans:'O(N)'
+    },
+    {
+        que:'What is the Average and Worst Case Time Complexity to search an element in Stack ?',
+        ans:'O(N)'
+    },
+    {
+        que:'What is the Average and Worst Case Time Complexity to insert an element in Stack?',
+        ans:'O(1)'
+    },
+    {
+        que:'What is the Average and Worst Case Time Complexity to delete an element from Stack?',
+        ans:'O(1)'
+    },
+    {
+        que:'What is the Average and Worst Case Time Complexity to access an element in Queue?',
+        ans:'O(N)'
+    },
+    {
+        que:'What is the Average and Worst Case Time Complexity to search an element in Queue ?',
+        ans:'O(N)'
+    },
+    {
+        que:'What is the Average and Worst Case Time Complexity to insert an element in Queue?',
+        ans:'O(1)'
+    },
+    {
+        que:'What is the Average and Worst Case Time Complexity to delete an element from Queue?',
+        ans:'O(1)'
+    },
+    {
+        que:'What is the Average and Worst Case Time Complexity to access an element in Singly Linked List?',
+        ans:'O(N)'
+    },
+    {
+        que:'What is the Average and Worst Case Time Complexity to search an element in Singly Linked List ?',
+        ans:'O(N)'
+    },
+    {
+        que:'What is the Average and Worst Case Time Complexity to insert an element in Singly Linked List?',
+        ans:'O(1)'
+    },
+    {
+        que:'What is the Average and Worst Case Time Complexity to delete an element from Singly Linked List?',
+        ans:'O(1)'
+    },
+    {
+        que:'What is the Average and Worst Case Time Complexity to access an element in Doubly Linked List?',
+        ans:'O(N)'
+    },
+    {
+        que:'What is the Average and Worst Case Time Complexity to search an element in Doubly Linked List ?',
+        ans:'O(N)'
+    },
+    {
+        que:'What is the Average and Worst Case Time Complexity to insert an element in Doubly Linked List?',
+        ans:'O(1)'
+    },
+    {
+        que:'What is the Average Time Complexity to delete an element from Doubly Linked List?',
+        ans:'O(1)'
+    },
+    {
+        que:'What is the Average Time Complexity to access an element in Binary Search Tree?',
+        ans:'O(Log N)'
+    },
+    {
+        que:'What is the Average Time Complexity to search an element in Binary Search Tree ?',
+        ans:'O(Log N)'
+    },
+    {
+        que:'What is the Average Time Complexity to insert an element in Binary Search Tree?',
+        ans:'O(Log N)'
+    },
+    {
+        que:'What is the Average Time Complexity to delete an element from Binary Search Tree?',
+        ans:'O(Log N)'
+    },
+    {
+        que:'What is the Average  and Worst Case Time Complexity to access an element in AVL Tree?',
+        ans:'O(Log N)'
+    },
+    {
+        que:'What is the Average and Worst Case  Time Complexity to search an element in AVL Tree ?',
+        ans:'O(Log N)'
+    },
+    {
+        que:'What is the Average and Worst Case  Time Complexity to insert an element in AVL Tree?',
+        ans:'O(Log N)'
+    },
+    {
+        que:'What is the Average and Worst Case  Time Complexity to delete an element from AVL Tree?',
+        ans:'O(Log N)'
+    },
+    {
+        que:'What is the Average Time Complexity to access an element in Binary Tree?',
+        ans:'O(Log N)'
+    },
+    {
+        que:'What is the Average Time Complexity to search an element in Binary Tree ?',
+        ans:'O(Log N)'
+    },
+    {
+        que:'What is the Average Time Complexity to insert an element in Binary Tree?',
+        ans:'O(Log N)'
+    },
+    {
+        que:'What is the Average Time Complexity to delete an element from Binary Tree?',
+        ans:'O(Log N)'
+    },
+    {
+        que:'What is the Average ans Worst Case  Time Complexity to access an element in Red Black Tree?',
+        ans:'O(Log N)'
+    },
+    {
+        que:'What is the Average ans Worst Case  Time Complexity to search an element in Red Black Tree ?',
+        ans:'O(Log N)'
+    },
+    {
+        que:'What is the Average ans Worst Case  Time Complexity to insert an element in Red Black Tree?',
+        ans:'O(Log N)'
+    },
+    {
+        que:'What is the Average ans Worst Case  Time Complexity to delete an element from Red Black Tree?',
+        ans:'O(Log N)'
+    },
+    {
+        que:'What is the Average Time Complexity to access an element in Hash Table?',
+        ans:'O(1)'
+    },
+    {
+        que:'What is the Average Time Complexity to search an element in Hash Table ?',
+        ans:'O(1)'
+    },
+    {
+        que:'What is the Average Time Complexity to insert an element in Hash Table?',
+        ans:'O(1)'
+    },
+    {
+        que:'What is the Average Time Complexity to delete an element from Hash Table?',
+        ans:'O(1)'
+    },
+    {
+        que:'What is the Worst Case Time Complexity to access an element in Hash Table?',
+        ans:'O(N)'
+    },
+    {
+        que:'What is the Worst Case Time Complexity to search an element in Hash Table ?',
+        ans:'O(N)'
+    },
+    {
+        que:'What is the Worst Case Time Complexity to insert an element in Hash Table?',
+        ans:'O(N)'
+    },
+    {
+        que:'What is the Worst Case Time Complexity to delete an element from Hash Table?',
+        ans:'O(N)'
+    },
+    {
+        que:'What is the Worst Case Time Complexity to access an element in Binary Search Tree?',
+        ans:'O(N)'
+    },
+    {
+        que:'What is the Worst Case Time Complexity to search an element in Binary Search Tree ?',
+        ans:'O(N)'
+    },
+    {
+        que:'What is the Worst Case Time Complexity to insert an element in Binary Search Tree?',
+        ans:'O(N)'
+    },
+    {
+        que:'What is the Worst Case Time Complexity to delete an element from Binary Search Tree?',
+        ans:'O(N)'
+    },
+    {
+        que:'What is the Worst Case Time Complexity to access an element in Binary Tree?',
+        ans:'O(N)'
+    },
+    {
+        que:'What is the Worst Case Time Complexity to search an element in Binary Tree ?',
+        ans:'O(N)'
+    },
+    {
+        que:'What is the Worst Case Time Complexity to insert an element in Binary Tree?',
+        ans:'O(N)'
+    },
+    {
+        que:'What is the Worst Case Time Complexity to delete an element from Binary Tree?',
+        ans:'O(N)'
+    },
+    {
+        que:'What is the Best Case Time Complexity of Selection Sort?',
+        ans:'Ω(N²)',
+      
+        
+    },
+    {
+        que:'What is the Average Case Time Complexity of Selection Sort?',
+        ans:'Θ(N²)',
+      
+        
+    },
+    {
+        que:'What is the Worst Case Time Complexity of Selection Sort?',
+        ans:'O(N²)',
+      
+        
+    },
+    {
+        que:'What is the Space Complexity of Selection Sort?',
+        ans:'O(1)',
+      
+        
+    },
+    {
+        que:'What is the Best Case Time Complexity of Bubble Sort?',
+        ans:'Ω(N)',
+       
+        
+    },
+    {
+        que:'What is the Average Case Time Complexity of Bubble Sort?',
+        ans:'Θ(N²)',
+       
+        
+    },
+    {
+        que:'What is the Worst Case Time Complexity of Bubble Sort?',
+        ans:'O(N²)',
+       
+        
+    },
+    {
+        que:'What is the Space Complexity of Bubble Sort?',
+        ans:'O(1)',
+       
+        
+    },
+    {
+        que:'What is the Best Case Time Complexity of Insertion Sort?',
+        ans:'Ω(N)',
+       
+        
+    },
+    {
+        que:'What is the Average Case Time Complexity of Insertion Sort?',
+        ans:'Θ(N²)',
+       
+        
+    },
+    {
+        que:'What is the Worst Case Time Complexity of Insertion Sort?',
+        ans:'O(N²)',
+       
+        
+    },
+    {
+        que:'What is the Space Complexity of Insertion Sort?',
+        ans:'O(1)',
+       
+        
+    },
+    {
+        que:'What is the Best Case Time Complexity of Heap Sort?',
+        ans:'Ω(N Log(N))',
+        
+        
+    },
+    {
+        que:'What is the Average Case Time Complexity of Heap Sort?',
+        ans:'Θ(N Log(N))',
+        
+        
+    },
+    {
+        que:'What is the Worst Case Time Complexity of Heap Sort?',
+        ans:'O(N Log(N))',
+        
+        
+    },
+    {
+        que:'What is the Space Complexity of Heap Sort?',
+        ans:'O(1)',
+        
+        
+    },
+    {
+        que:'What is the Best Case Time Complexity of Quick Sort?',
+        ans:'Ω(N Log(N))',
+        
+        
+    },
+    {
+        que:'What is the Average Case Time Complexity of Quick Sort?',
+        ans:'Θ(N Log(N))',
+        
+        
+    },
+    {
+        que:'What is the Worst Case Time Complexity of Quick Sort?',
+        ans:'O(N²)',
+        
+        
+    },
+    {
+        que:'What is the Average Case Space Complexity of Quick Sort?',
+        ans:'O(Log N)',
+       
+        
+    },
+    {
+        que:'What is the Worst Case Space Complexity of Quick Sort?',
+        ans:'O(N)',
+       
+        
+    },
+    {
+        que:'What is the Best Case Time Complexity of Merge Sort?',
+        ans:'Ω(N Log(N))',
+        
+        
+    },
+    {
+        que:'What is the Average Case Time Complexity of Merge Sort?',
+        ans:'Θ(N Log(N))',
+        
+        
+    },
+    {
+        que:'What is the Worst Case Time Complexity of Merge Sort?',
+        ans:'O(N Log(N))',
+        
+        
+    },
+    {
+        que:'What is the Space Complexity of Merge Sort?',
+        ans:'O(N)',
+        
+        
+    },
+    {
+        que:'What is the Best Case Time Complexity of Bucket Sort?',
+        ans:'Ω(N+K)',
+        
+        
+    },
+    {
+        que:'What is the Average Case Time Complexity of Bucket Sort?',
+        ans:'Θ(N+K)',
+        
+        
+    },
+    {
+        que:'What is the Worst Case Time Complexity of Bucket Sort?',
+        ans:'O(N²)',
+        
+        
+    },
+    {
+        que:'What is the Space Complexity of Bucket Sort?',
+        ans:'O(N+K)',
+        
+        
+    },
+    {
+        que:'What is the Best Case Time Complexity of Count Sort?',
+        ans:'Ω(N+K)',
+        
+        
+    },
+    {
+        que:'What is the Average Case Time Complexity of Count Sort?',
+        ans:'Θ(N+k)',
+        
+        
+    },
+    {
+        que:'What is the Worst Case Time Complexity of Count Sort?',
+        ans:'O(N+k)',
+        
+        
+    },
+    {
+        que:'What is the Space Complexity of Count Sort?',
+        ans:'O(N+K)',
+        
+        
+    }
+    
+]
+
+let getQue = document.getElementById('que');
+let getBtn = document.getElementById('ans-btn');
+let getAns = document.getElementById('ans');
+
+let index = Math.floor(Math.random() * array.length)
+
+getQue.innerHTML = array[index].que;
+
+getBtn.addEventListener('click' , () => {
+    getAns.innerHTML = `Answer : ${array[index].ans}`
+  if(getBtn.innerHTML==='View Answer'){
+      getAns.classList.add('show');
+      if(getAns.classList.contains('hide')){
+          getAns.classList.remove('hide')
+      }
+      getBtn.innerHTML = 'Hide Answer';
+  }  
+  else{
+      getAns.classList.add('hide');
+      if(getAns.classList.contains('show')){
+        getAns.classList.remove('show')
+    }
+      getBtn.innerHTML = 'View Answer'
+  }
+})
